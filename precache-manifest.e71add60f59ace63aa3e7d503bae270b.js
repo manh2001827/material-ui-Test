1d970bf9c2d7ee52250c07c02d4ef719778660b8
manh2001827 <66905530+manh2001827@users.noreply.github.com>
2021-09-14 20:09:29 +0700

    Updates

---
+++ precache-manifest.e71add60f59ace63aa3e7d503bae270b.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b88a9100a05ccabc6ef2cca5b9b77c8c",
+    "url": "/material-ui-Test/index.html"
+  },
+  {
+    "revision": "e86090e7675011db4a05",
+    "url": "/material-ui-Test/static/css/2.9391108c.chunk.css"
+  },
+  {
+    "revision": "e86090e7675011db4a05",
+    "url": "/material-ui-Test/static/js/2.f0093bb1.chunk.js"
+  },
+  {
+    "revision": "9efecc6e366a89e935d3722c67df4853",
+    "url": "/material-ui-Test/static/js/2.f0093bb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae6c4a2ad3154a02c029",
+    "url": "/material-ui-Test/static/js/main.17a74cc3.chunk.js"
+  },
+  {
+    "revision": "296d7d1164bdde08b97b",
+    "url": "/material-ui-Test/static/js/runtime-main.a9766965.js"
+  },
+  {
+    "revision": "2bea765693948ba9da2a6f66d693cfb9",
+    "url": "/material-ui-Test/static/media/0.2bea7656.png"
+  },
+  {
+    "revision": "95066ca445096bb77c820fa1b5fa689e",
+    "url": "/material-ui-Test/static/media/0.95066ca4.jpg"
+  },
+  {
+    "revision": "0882c8c9b58015d20f0da9798a90da3f",
+    "url": "/material-ui-Test/static/media/1.0882c8c9.jpg"
+  },
+  {
+    "revision": "116af8f9285213c805ec8137464a6a30",
+    "url": "/material-ui-Test/static/media/1.116af8f9.jpg"
+  },
+  {
+    "revision": "1daab241e169eece79caba213bb6f63d",
+    "url": "/material-ui-Test/static/media/1.1daab241.png"
+  },
+  {
+    "revision": "95066ca445096bb77c820fa1b5fa689e",
+    "url": "/material-ui-Test/static/media/1.95066ca4.jpg"
+  },
+  {
+    "revision": "b7a3d807d5f8bfb2e27691f66964944c",
+    "url": "/material-ui-Test/static/media/1.b7a3d807.png"
+  },
+  {
+    "revision": "b97aa98b6e6c887f203b3169a974414b",
+    "url": "/material-ui-Test/static/media/1.b97aa98b.png"
+  },
+  {
+    "revision": "d33b6b85c1fa91ca59ba255ead0336e7",
+    "url": "/material-ui-Test/static/media/1.d33b6b85.png"
+  },
+  {
+    "revision": "d9bd7b3c9f40a21131803087aba06360",
+    "url": "/material-ui-Test/static/media/1.d9bd7b3c.png"
+  },
+  {
+    "revision": "4163ab952f6dd3f5308c2faaa9b16fcb",
+    "url": "/material-ui-Test/static/media/2.4163ab95.png"
+  },
+  {
+    "revision": "41ce4b752f46a5660bf60eb4eb78cdeb",
+    "url": "/material-ui-Test/static/media/2.41ce4b75.jpg"
+  },
+  {
+    "revision": "47ec8861b91b12f3220d719db08a64df",
+    "url": "/material-ui-Test/static/media/2.47ec8861.png"
+  },
+  {
+    "revision": "5bcbc576882429c5ee5aab8992d3c0c1",
+    "url": "/material-ui-Test/static/media/2.5bcbc576.jpg"
+  },
+  {
+    "revision": "ec0633256cd57f65aff89075696b858a",
+    "url": "/material-ui-Test/static/media/2.ec063325.jpg"
+  },
+  {
+    "revision": "41ce4b752f46a5660bf60eb4eb78cdeb",
+    "url": "/material-ui-Test/static/media/3.41ce4b75.jpg"
+  },
+  {
+    "revision": "86d39baf15e700c8d9db5d826a0ce394",
+    "url": "/material-ui-Test/static/media/3.86d39baf.jpg"
+  },
+  {
+    "revision": "acab11a0abdd9e3070d08bfc160fcf01",
+    "url": "/material-ui-Test/static/media/3.acab11a0.jpg"
+  },
+  {
+    "revision": "c347ef448b6129815da1ca6da8bcc657",
+    "url": "/material-ui-Test/static/media/3.c347ef44.png"
+  },
+  {
+    "revision": "dafb8059640f8f2fe3052e14eb3658a0",
+    "url": "/material-ui-Test/static/media/3.dafb8059.png"
+  },
+  {
+    "revision": "0183192e60cdfebee597ae3d41d6e48f",
+    "url": "/material-ui-Test/static/media/4.0183192e.png"
+  },
+  {
+    "revision": "3820ac5d3a885edd91d0e3a72a8bc565",
+    "url": "/material-ui-Test/static/media/4.3820ac5d.jpg"
+  },
+  {
+    "revision": "6bd795537ed41e928a5144830351ea19",
+    "url": "/material-ui-Test/static/media/5.6bd79553.jpg"
+  },
+  {
+    "revision": "e8f767a42d9f976d4c329f7ddab0785b",
+    "url": "/material-ui-Test/static/media/5.e8f767a4.png"
+  },
+  {
+    "revision": "5bcbc576882429c5ee5aab8992d3c0c1",
+    "url": "/material-ui-Test/static/media/6.5bcbc576.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/material-ui-Test/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/material-ui-Test/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/material-ui-Test/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/material-ui-Test/static/media/slick.f97e3bbf.svg"
+  }
+]);
